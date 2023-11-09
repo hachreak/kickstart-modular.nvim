872af5ac1fe9595c88c2f9f423b409c76ae81387
Leonardo Rossi <leonardo.rossi@unipr.it>
2023-11-09 16:01:40 +0100

    plugins: default config
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,46 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+--
+
+vim.o.hlsearch = true
+
+-- Remove trailing spaces
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
+
+-- map tabnew
+vim.api.nvim_set_keymap('i', '<C-t>', '<Esc>:tabnew<CR>', {
+  noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-t>', ':tabnew<CR>', {
+  noremap = true, silent = true })
+-- map tab left-right
+vim.api.nvim_set_keymap('', '<C-Left>', '<Esc>:tabprev<CR>', {
+  noremap = true, silent = true })
+vim.api.nvim_set_keymap('', '<C-Right>', '<Esc>:tabnext<CR>', {
+  noremap = true, silent = true })
+-- map close
+vim.api.nvim_set_keymap('', '<C-q>', '<Esc>:tabclose<CR>', {
+  noremap = true, silent = true })
+
+-- copy to end of line using
+vim.api.nvim_set_keymap('n', 'Y', 'y$', {
+  noremap = true, silent = true })
+
+-- Enable mouse mode
+vim.o.mouse = 'i'
+
+-- Move to another window
+vim.api.nvim_set_keymap('', '<C-j>', '<C-w>j', {
+  noremap = true, silent = true })
+vim.api.nvim_set_keymap('', '<C-k>', '<C-w>k', {
+  noremap = true, silent = true })
+vim.api.nvim_set_keymap('', '<C-l>', '<C-w>l', {
+  noremap = true, silent = true })
+vim.api.nvim_set_keymap('', '<C-h>', '<C-w>h', {
+  noremap = true, silent = true })
+
+
 return {}
